cc7691e73d1b0c0ff5302dbeaa2cd39b6b076352
Eduardo Pilati <pilatieduardo@gmail.com>
2020-04-22 21:08:24 -0300

    Desenvolvimento

---
+++ Gerenciador/app/Http/Controllers/SolicitacoesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SolicitacoesController extends Controller
+{
+    public function index()
+    {
+        return "SolicitacoesController";
+    }
+}
